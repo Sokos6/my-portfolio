32b424dd3e7e773e9ad93d0d25105e8bff68d761
Alice Jowsey <12345alcatraz@gmail.com>
2020-08-08 20:48:54 +1200

    remove redux store

--- src/js/components/TestComponent.jsx
+++ src/js/components/TestComponent.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-//selectors
-import { getTestAction } from '../store/selectors/AppSelectors';
-//actions
-import { setTestAction } from '../store/actions/AppActions';
 
 const TestComponent = () => {
-	const dispatch = useDispatch(),
-		testAction = useSelector((state) => getTestAction({ state }));
-
 	return (
 		<div className="container" style={{ textAlign: 'center' }}>
 			<p>
-				Testing the store <strong>{testAction}</strong>
+				Testing the store <strong></strong>
 			</p>
-			<button className="std-btn primary" onClick={() => dispatch(setTestAction())}>
-				Change text
-			</button>
+			<button className="std-btn primary">Change text</button>
 		</div>
 	);
 };
