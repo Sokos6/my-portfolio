fe4ecc99a39892e20cd4e40ed8cc915ff7453348
Alice Jowsey <12345alcatraz@gmail.com>
2020-08-08 23:11:48 +1200

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import React, { lazy, Suspense } from 'react';
 import { hot } from 'react-hot-loader/root';
+import { Route, Switch } from 'react-router-dom';
+//Pages
+import HomePage from './js/containers/pages/HomePage';
+import AboutPage from './js/containers/pages/AboutPage';
+import SkillsPage from './js/containers/pages/SkillsPage';
+import ProjectsPage from './js/containers/pages/ProjectsPage';
+import ContactsPage from './js/containers/pages/ContactsPage';
+import NotFoundPage from './js/containers/pages/NotFoundPage';
 //components
 import LoadingIndicator from './js/components/UI/LoadingIndicator';
 const AppHeader = lazy(() => import('./js/containers/navigation/AppHeader'));
@@ -13,6 +21,14 @@ const App = () => (
 		}
 	>
 		<AppHeader />
+		<Switch>
+			<Route path="/" exact component={HomePage} />
+			<Route path="/about" component={AboutPage} />
+			<Route path="/skills" component={SkillsPage} />
+			<Route path="/projects" component={ProjectsPage} />
+			<Route path="/contacts" component={ContactsPage} />
+			<Route path="*" component={NotFoundPage} />
+		</Switch>
 	</Suspense>
 );
 
